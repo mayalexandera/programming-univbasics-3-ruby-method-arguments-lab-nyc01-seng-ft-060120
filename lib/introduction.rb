05cc7aea2ae4570d71b0e93b8a78d35ad7de9257
Maya Blevins <maya.blevins@gmail.com>
2020-05-04 17:37:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,10 +13,12 @@ end
 
 
 def introduction_with_language(name, language)
-  introduction(Maya)
-  puts "and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
